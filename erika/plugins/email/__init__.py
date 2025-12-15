cf772d296caf741e7c45cd2437d184466af83d10
ChickenPwny <chickenpwny@users.noreply.github.com>
2025-12-14 20:34:14 -0500

    Update email plugin __init__.py to export new providers

--- erika/plugins/email/__init__.py
+++ erika/plugins/email/__init__.py
@@ -1,31 +1,44 @@
 #!/usr/bin/env python3
 """
-Erika Email Plugin
-==================
+Erika Email Plugin Package
+==========================
 
-Gmail OAuth integration for email processing.
-
-Author: Living Archive team
-Version: 1.0.0
+Email integration for Erika supporting multiple providers:
+- Gmail (OAuth2)
+- IMAP (generic email servers)
 """
 
-from .gmail_service import ErikaGmailService, get_gmail_service_for_user
-
-# check_gmail_for_user may not exist - make it optional
-try:
-    from .gmail_service import check_gmail_for_user
-except ImportError:
-    def check_gmail_for_user(*args, **kwargs):
-        """Placeholder if function doesn't exist"""
-        return False
+from .base_email_service import (
+    BaseEmailService,
+    EmailServiceError,
+    EmailAuthenticationError,
+    EmailValidationError
+)
+from .gmail_service import ErikaGmailService
+from .imap_service import IMAPService
 from .oauth_token_manager import OAuthTokenManager
 from .plugin import EmailPlugin
 
+# Legacy exports for backward compatibility
+try:
+    from .gmail_service import get_gmail_service_for_user, check_gmail_for_user
+except ImportError:
+    def get_gmail_service_for_user(*args, **kwargs):
+        """Placeholder if function doesn't exist"""
+        return None
+    def check_gmail_for_user(*args, **kwargs):
+        """Placeholder if function doesn't exist"""
+        return False
+
 __all__ = [
+    'BaseEmailService',
+    'EmailServiceError',
+    'EmailAuthenticationError',
+    'EmailValidationError',
     'ErikaGmailService',
+    'IMAPService',
     'OAuthTokenManager',
     'EmailPlugin',
     'get_gmail_service_for_user',
     'check_gmail_for_user',
 ]
-
